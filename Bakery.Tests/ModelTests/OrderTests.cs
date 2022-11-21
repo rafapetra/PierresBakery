65725b2886681675c01808c60c2af2f943b425ac
Rafael Petrachini <raffp@me.com>
2022-11-20 17:57:02 -0800

    pass second test - verify the Bread can hold data for 'amount' property

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -10,8 +10,18 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreateInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(2);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
+    [TestMethod]
+    public void GetAmount_ReturnsAmount_Int()
+
+    {
+      int GetAmount = 2;
+      Bread newBread = new Bread(GetAmount);
+      int result = newBread.Amount;
+      Assert.AreEqual(GetAmount, result);
     }
 
   }
